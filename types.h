91a6d4f86916f47816458248d621db02183aa47a
Bjarni R. Einarsson <bre@klaki.net>
2012-02-24 10:02:33 +0000

    Needed for sha1

---
+++ types.h
@@ -0,0 +1,7 @@
+typedef signed char               int8_t;
+typedef short int                 int16_t;
+typedef int                       int32_t;
+
+typedef unsigned char             uint8_t;
+typedef unsigned short int        uint16_t;
+typedef unsigned int              uint32_t;
