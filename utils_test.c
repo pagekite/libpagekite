a5b17f664b8e9bcbfaa2ab9c1aec96d06f20133e
Bjarni R. Einarsson <bre@klaki.net>
2012-06-28 11:16:35 +0000

    Fixing tests

--- utils_test.c
+++ utils_test.c
@@ -19,6 +19,7 @@ along with this program.  If not, see: <http://www.gnu.org/licenses/>
 ******************************************************************************/
 #include <assert.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "utils.h"
 
