21850b761ffe51e30839ebe0540d334e796c1375
Bjarni R. Einarsson <bre@klaki.net>
2013-11-25 18:45:52 +0000

    Updated Copyright notice

--- pkconn.h
+++ pkconn.h
@@ -1,7 +1,7 @@
 /******************************************************************************
 pkconn.h - Connection objects
 
-This file is Copyright 2011, 2012, The Beanstalks Project ehf.
+This file is Copyright 2011-2013, The Beanstalks Project ehf.
 
 This program is free software: you can redistribute it and/or modify it under
 the terms  of the  Apache  License 2.0  as published by the  Apache  Software
@@ -9,8 +9,7 @@ Foundation.
 
 This program is distributed in the hope that it will be useful,  but  WITHOUT
 ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
-details.
+FOR A PARTICULAR PURPOSE.  See the Apache License for more details.
 
 You should have received a copy of the Apache License along with this program.
 If not, see: <http://www.apache.org/licenses/>
