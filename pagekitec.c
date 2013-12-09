1dd8688c31ce7b7963f91f6dda2ee7b01a3e0004
Bjarni R. Einarsson <bre@klaki.net>
2013-12-09 14:35:16 +0000

    Fix -6/-4 usage strings, added -n

--- pagekitec.c
+++ pagekitec.c
@@ -37,7 +37,7 @@ Note: For alternate license terms, see the file COPYING.md.
 #include <signal.h>
 
 
-void usage(void) {
+void usage(int ecode) {
   fprintf(stderr, "This is pagekitec.c from libpagekite %s.\n\n", PK_VERSION);
   fprintf(stderr, "Usage:\tpagekitec [options] LPORT PROTO"
                                    " NAME.pagekite.me PPORT SECRET ...\n"
@@ -47,12 +47,14 @@ void usage(void) {
                   "\t-I\tConnect insecurely, without SSL.\n"
                   "\t-S\tStatic setup, disable FE failover and DDNS updates\n"
                   "\t-c N\tSet max connection count to N (default = 25)\n"
+                  "\t-n N\tAlways connect to N spare frontends (default = 0)\n"
                   "\t-B N\tBail out (abort) after N logged errors\n"
                   "\t-E N\tAllow eviction of streams idle for >N seconds\n"
                   "\t-R\tChoose frontends at random, instead of pinging\n"
-                  "\t-6\tDisable IPv4 frontends\n"
-                  "\t-4\tDisable IPv6 frontends\n"
+                  "\t-4\tDisable IPv4 frontends\n"
+                  "\t-6\tDisable IPv6 frontends\n"
                   "\n");
+  exit(ecode);
 }
 
 void raise_log_level(int sig) {
@@ -71,6 +73,7 @@ int main(int argc, char **argv) {
   int use_ipv6 = 1;
   int use_ssl = 1;
   int max_conns = 25;
+  int spare_frontends = 0;
   char* ddns_url = PAGEKITE_NET_DDNS;
   int ac;
   int pport;
@@ -81,7 +84,7 @@ int main(int argc, char **argv) {
   srand(time(0) ^ getpid());
   pks_global_init(PK_LOG_NORMAL);
 
-  while (-1 != (ac = getopt(argc, argv, "46c:B:E:IqRSv"))) {
+  while (-1 != (ac = getopt(argc, argv, "46c:B:E:In:qRSv"))) {
     switch (ac) {
       case '4':
         use_ipv4 = 0;
@@ -107,26 +110,29 @@ int main(int argc, char **argv) {
       case 'B':
         gotargs++;
         if (1 == sscanf(optarg, "%u", &pk_state.bail_on_errors)) break;
+        usage(1);
       case 'c':
         gotargs++;
         if (1 == sscanf(optarg, "%d", &max_conns)) break;
+        usage(1);
       case 'E':
         gotargs++;
         if (1 == sscanf(optarg, "%u", &tmp_uint)) {
           pk_state.conn_eviction_idle_s = tmp_uint;
           break;
         }
+        usage(1);
+      case 'n':
+        gotargs++;
+        if (1 == sscanf(optarg, "%d", &spare_frontends)) break;
+        usage(1);
       default:
-        usage();
-        exit(1);
+        usage(1);
     }
     gotargs++;
   }
 
-  if ((argc-1-gotargs) < 5 || ((argc-1-gotargs) % 5) != 0) {
-    usage();
-    exit(1);
-  }
+  if ((argc-1-gotargs) < 5 || ((argc-1-gotargs) % 5) != 0) usage(1);
 
   signal(SIGUSR1, &raise_log_level);
   pk_state.log_mask = ((verbosity < 0) ? PK_LOG_ERRORS :
@@ -147,12 +153,12 @@ int main(int argc, char **argv) {
     pk_perror(argv[0]);
     exit(1);
   }
+  m->want_spare_frontends = spare_frontends;
 
   for (ac = gotargs; ac+5 < argc; ac += 5) {
     if ((1 != sscanf(argv[ac+1], "%d", &lport)) ||
         (1 != sscanf(argv[ac+4], "%d", &pport))) {
-      usage();
-      exit(2);
+      usage(2);
     }
     proto = argv[ac+2];
     kitename = argv[ac+3];
