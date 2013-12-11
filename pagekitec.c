d10a5b9c7cc225657d171d175e985eb9134f2643
Bjarni R. Einarsson <bre@klaki.net>
2013-12-11 15:03:28 +0000

    Add -C and -F

--- pagekitec.c
+++ pagekitec.c
@@ -50,9 +50,11 @@ void usage(int ecode) {
                   "\t-n N\tAlways connect to N spare frontends (default = 0)\n"
                   "\t-B N\tBail out (abort) after N logged errors\n"
                   "\t-E N\tAllow eviction of streams idle for >N seconds\n"
+                  "\t-F x\tUse x (a DNS name) as frontend pool\n"
                   "\t-R\tChoose frontends at random, instead of pinging\n"
                   "\t-4\tDisable IPv4 frontends\n"
                   "\t-6\tDisable IPv6 frontends\n"
+                  "\t-C\tDisable auto-adding current DNS IP as a front-end\n"
                   "\n");
   exit(ecode);
 }
@@ -71,9 +73,11 @@ int main(int argc, char **argv) {
   int verbosity = 0;
   int use_ipv4 = 1;
   int use_ipv6 = 1;
+  int use_current = 1;
   int use_ssl = 1;
   int max_conns = 25;
   int spare_frontends = 0;
+  char* fe_hostname = NULL;
   char* ddns_url = PAGEKITE_NET_DDNS;
   int ac;
   int pport;
@@ -84,13 +88,16 @@ int main(int argc, char **argv) {
   srand(time(0) ^ getpid());
   pks_global_init(PK_LOG_NORMAL);
 
-  while (-1 != (ac = getopt(argc, argv, "46c:B:E:In:qRSv"))) {
+  while (-1 != (ac = getopt(argc, argv, "46c:B:CE:F:In:qRSv"))) {
     switch (ac) {
       case '4':
         use_ipv4 = 0;
         break;
       case '6':
         use_ipv6 = 0;
+        break;
+      case 'C':
+        use_current = 0;
         break;
       case 'v':
         verbosity++;
@@ -106,6 +113,10 @@ int main(int argc, char **argv) {
         break;
       case 'S':
         ddns_url = NULL;
+        break;
+      case 'F':
+        gotargs++;
+        fe_hostname = strdup(optarg);
         break;
       case 'B':
         gotargs++;
@@ -165,17 +176,24 @@ int main(int argc, char **argv) {
     secret = argv[ac+5];
     if ((NULL == (pkm_add_kite(m, proto, kitename, 0, secret,
                                "localhost", lport))) ||
-        (0 > (pkm_add_frontend(m, kitename, pport, FE_STATUS_AUTO)))) {
+        (use_current &&
+         (0 > (pkm_add_frontend(m, kitename, pport, FE_STATUS_AUTO))))) {
       pk_perror(argv[0]);
       exit(3);
     }
   }
 
   if (ddns_url != NULL) {
-    if (((use_ipv4) &&
-         (0 > (pkm_add_frontend(m, PAGEKITE_NET_V4FRONTENDS, FE_STATUS_AUTO)))) ||
-        ((use_ipv6) &&
-         (0 > (pkm_add_frontend(m, PAGEKITE_NET_V6FRONTENDS, FE_STATUS_AUTO))))) {
+    if (fe_hostname) {
+      if (0 > (pkm_add_frontend(m, fe_hostname, 443, FE_STATUS_AUTO))) {
+        pk_perror(argv[0]);
+        exit(4);
+      }
+    }
+    else if (((use_ipv4) &&
+              (0 > (pkm_add_frontend(m, PAGEKITE_NET_V4FRONTENDS, FE_STATUS_AUTO)))) ||
+             ((use_ipv6) &&
+              (0 > (pkm_add_frontend(m, PAGEKITE_NET_V6FRONTENDS, FE_STATUS_AUTO))))) {
       pk_perror(argv[0]);
       exit(4);
     }
