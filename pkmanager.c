d724b713f59c526e215ba4c504152068f0705939
Bjarni R. Einarsson <bre@klaki.net>
2013-12-16 12:46:13 +0000

    Move Windows libev wrapper to own .c file

--- pkmanager.c
+++ pkmanager.c
@@ -28,10 +28,6 @@ Note: For alternate license terms, see the file COPYING.md.
 #include "pkmanager.h"
 #include "pklogging.h"
 
-#ifdef _MSC_VER
-#include "libev/ev.c"
-#endif
-
 
 void pkm_yield(struct pk_manager *pkm)
 {
