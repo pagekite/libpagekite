272c276dc48579dbc26c9d8685e8362e16c9d86d
Bjarni R. Einarsson <bre@klaki.net>
2019-12-11 14:52:12 +0000

    Call this v0.91.191211

--- include/pagekite.h
+++ include/pagekite.h
@@ -31,12 +31,12 @@ WARNING: This file is processed automatically to generate the JNI and API
 
 /* Constants: Libpagekite version */
 #ifdef ANDROID
-#define PK_VERSION "0.91.190530A" /* Note: Update frontend DNS on changes! */
+#define PK_VERSION "0.91.191211A" /* Note: Update frontend DNS on changes! */
 #else
 #ifdef _MSC_VER
-#define PK_VERSION "0.91.190530W"
+#define PK_VERSION "0.91.191211W"
 #else
-#define PK_VERSION "0.91.190530C"
+#define PK_VERSION "0.91.191211C"
 #endif
 #endif
 
